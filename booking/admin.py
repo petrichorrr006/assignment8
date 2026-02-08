b8870f7462754abc7c706311556b2544950e2ca2
petrichorrr006 <raven24112006@gmail.com>
2026-02-08 23:45:23 +0500

    .

--- booking/admin.py
+++ booking/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
-from .models import Provider, TimeSlot, Booking
+from .models import Provider, TimeSlot, Booking, PatientProfile
+
+
+@admin.register(PatientProfile)
+class PatientProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'phone_number', 'is_phone_verified')
+    list_filter = ('is_phone_verified',)
+    search_fields = ('user__username', 'phone_number')
 
 @admin.register(Provider)
 class ProviderAdmin(admin.ModelAdmin):
-    list_display = ('user', 'specialization')
-    search_fields = ('user__username',)
+    list_display = ('user', 'specialization', 'photo')
+    list_filter = ('specialization',)
+    search_fields = ('user__username', 'user__first_name', 'user__last_name')
 
 @admin.register(TimeSlot)
 class TimeSlotAdmin(admin.ModelAdmin):
@@ -13,4 +21,5 @@ class TimeSlotAdmin(admin.ModelAdmin):
 
 @admin.register(Booking)
 class BookingAdmin(admin.ModelAdmin):
-    list_display = ('client', 'slot', 'created_at')
+    list_display = ('client', 'slot', 'status', 'created_at')
+    list_filter = ('status',)
--- myproject/settings.py
+++ myproject/settings.py
@@ -117,5 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+
+# Media files (user-uploaded images)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
