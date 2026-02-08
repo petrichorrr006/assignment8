17094a6b080049b2c31e76200faafc388648e3ca
petrichorrr006 <raven24112006@gmail.com>
2026-02-09 00:37:19 +0500

    Update subproject commit reference and clean up imports in views.py

--- booking/views.py
+++ booking/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import Provider, TimeSlot, Booking
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required
+
 import json
 
 
